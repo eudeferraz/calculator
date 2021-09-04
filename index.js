5d9d7cbf746d0513ebd0f3f64938cd7263448c24
Camila Ferraz <camicferraz@hotmail.com>
2021-09-04 18:54:18 -0300

    Updated - removed all the logic from the code

--- index.js
+++ index.js
@@ -1,52 +1,31 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
+const numbersButton = document.querySelectorAll('[data-num]');
+const operatorsButton = document.querySelectorAll('[data-ops]');
+const clearButton = document.getElementById('clear');
+const viewerTextElement = document.getElementById('viewer')
+const equalsButton = document.querySelector('equals');
+const numberZeroButton = document.querySelector('number-zero');
+const firstOperandTextElement = document.getElementById('firstElement');
+const secondOperandTextElement = document.getElementById('secondElement');
 
-function showValue(number) {
-  const element = document.getElementById('viewer')
-  element.innerHTML = number
+const displayVal = '';
+
+const updateDisplayValue = (clickBtn) => {
+  const btnText = clickBtn.target.innerText;
+
+  viewerTextElement.innerText = btnText;
 }
 
-// function fixOperatorButton(elementId) {
-//   const element = document.getElementById(`${elementId}`)
-//   element.style.removeProperty('background-color')
-//   element.style.backgroundColor = '#fafaf5'
-//   element.style.color = 'orange'
-// }
-
-
-const keys = document.querySelector('.calculator')
-keys.addEventListener('click', (event) => {
-  const {target} = event
-  if (!target.matches('button')){
-    return
-  }
-
-  if (target.classList.contains('number')){
-    showValue(target.innerHTML)
-    return
-  }
-
-  if (target.classList.contains('ops')){
-    console.log(target.id)
-    return
-  }
-
-  if (target.classList.contains('number-zero')){
-    console.log('operator', target.innerHTML)
-    return
-  }
-
-  if (target.classList.contains('equals')){
-    console.log('operator', target.innerHTML)
-    return
-  }
-
-  if (target.classList.contains('clear')){
-    console.log('operator', target.innerHTML)
-    return
-  }
+numbersButton.forEach(button => {
+  button.addEventListener('click', updateDisplayValue, false)
 })
+
+// operatorsButton.forEach(button => {
+//   button.addEventListener('click', () => {
+//     calculator.getOperator(button.innerText);
+//   })
+// })
+
+// clearButton.addEventListener('click', () => {
+//   calculator.clear();
+//   calculator.updateDisplay();
+// })
